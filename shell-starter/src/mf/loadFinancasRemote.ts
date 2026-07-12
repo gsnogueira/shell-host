e67bc9ee255a32313824b054a711da17459ad116
Gabriel Nogueira <gs.gamestudy@gmail.com>
2026-07-12 00:55:03 -0300

    clean code

--- shell-starter/src/mf/loadFinancasRemote.ts
+++
@@ -1,7 +0,0 @@
-import type { FinancasRemoteApi } from './types'
-
-export async function loadFinancasRemote(): Promise<FinancasRemoteApi> {
-  throw new Error(
-    'Module Federation ainda nao configurado. Use esta funcao quando a Fase 2 for ativada.',
-  )
-}
